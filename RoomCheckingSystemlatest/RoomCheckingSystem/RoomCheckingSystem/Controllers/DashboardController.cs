d8c9d536ff4d659eaca159d161adc827580bad94
SHAHBAZ-DEV\pral <shahbaz.naseer@pral.com.pk>
2021-06-15 16:31:42 +0500

    somechanges

--- RoomCheckingSystemlatest/RoomCheckingSystem/RoomCheckingSystem/Controllers/DashboardController.cs
+++ RoomCheckingSystemlatest/RoomCheckingSystem/RoomCheckingSystem/Controllers/DashboardController.cs
@@ -197,7 +197,7 @@ namespace RoomCheckingSystem.Controllers
         public void icontypedropdown()
         {
             var dropdownlist = util.TostatusprorityhList();
-            var obj = dropdownlist.ElementAt(1);
+            var obj = dropdownlist.ElementAt(0);
             obj.Selected = true;
             
             ViewBag.proritylist = dropdownlist;
